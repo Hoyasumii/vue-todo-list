6a3fff3f47c525d2f87e9f581a2c708a8a38b2cd
Alan Reis <alanreisanjo@gmail.com>
2025-09-23 10:15:25 -0300

    Cocô

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -9,7 +9,7 @@ defineProps<Props>();
 
 <template>
   <div
-    class="relative group rounded-xl duration-300 transition-all border border-stone-700 bg-gray-900 hover:border-emerald-700 focus-within:border-emerald-600 p-2">
+    class="relative group rounded-xl duration-300 transition-all border border-stone-700 bg-gray-950 hover:border-emerald-400/40 inset-shadow-sm inset-shadow-emerald-900 focus-within:border-emerald-300 focus-within:hover:border-emerald-400 p-2">
     <div v-if="placeholder" class="absolute">{{ placeholder }}</div>
     <input class="outline-0 w-full" type="text" :name="name" :v-model="name">
   </div>
